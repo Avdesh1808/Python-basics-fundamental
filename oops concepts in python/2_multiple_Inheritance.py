32cc34769a907ed8b37205481d67fe995d7ab58c
Avdesh Chauhan <avdeshchauhansingh@gmail.com>
2025-09-03 19:54:28 +0530

    first commit

---
+++ oops concepts in python/2_multiple_Inheritance.py
@@ -0,0 +1,20 @@
+class student:
+    def __init__(self,name):
+        self.name=name
+    def show(self):
+        print(f"{self.name} like dance.")
+class dance:
+    def __init__(self,name,dance):
+        self.name=name
+        self.dance_name=dance
+    def show(self):
+        print(f"{self.name} like dance.")
+        print(f"{self.dance_name}")
+class student_dance(student,dance):
+    def show_dance(self):
+        print("she like dance")
+a = student_dance("Nikita")
+a.show_dance()
+a.show()
+
+        
