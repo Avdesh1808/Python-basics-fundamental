9456d7a56d79d4d876f0babdab6241850707e8c1
Avdesh Chauhan <avdeshchauhansingh@gmail.com>
2025-09-03 18:49:37 +0530

    first commit

---
+++ oops concepts in python/1_Single_Inheritance.py
@@ -0,0 +1,16 @@
+class animal:
+    def __init__(self,name,species):
+        self.name = name
+        self.species = species
+    def make_sound(self):
+        print("every sounds made by animals")
+class Cat(animal):
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+    def make_sound(self):
+        print("Meooo !!")
+a=animal("Dog","Lebra")
+a.make_sound()
+b=Cat("Cat","Big Cat")
+b.make_sound()
