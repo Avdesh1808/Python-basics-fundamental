d97b199d9d0768aea65afb6e1783b17b663a1803
Avdesh Chauhan <avdeshchauhansingh@gmail.com>
2025-09-11 19:09:21 +0530

    I imported my created method and class from emp.py

---
+++ oops concepts in python/magic_Dunder_method.py
@@ -0,0 +1,8 @@
+# simply this dunder method is help to manipulate objects and their behaviour.
+# This is the code example of dunder method
+from emp import Employee
+e=Employee("Avdesh Chauhan")
+print(e)
+print(repr(e))
+print(len(e))
+e()#This function used for call a method which is already created in 'emp.py' file
